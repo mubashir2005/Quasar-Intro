617e2f79ffe506281941cbb3ebd937ceebb5e234
Mubashir <mubashirhasan716@gmail.com>
2022-05-25 20:41:24 +0530

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="wrapper">
+    {{registered ? (
+    <div class="homeWrapper">
+      <div class="main"></div>
+    </div>
+    ): (
+    <div class="singupContainer"></div>
+    )}}
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+});
+</script>
