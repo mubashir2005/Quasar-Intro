ed1e2754b1b2a3a7a94d37e915551733e1bdfaeb
Mubashir <mubashirhasan716@gmail.com>
2022-05-27 11:30:30 +0530

    will complete in sometime

---
+++ src/components/SignUp.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="wrapper">
+    <div class="logoContainer">
+      <img
+        src="https://cdn4.iconfinder.com/data/icons/logos-brands-5/24/vue-dot-js-512.png"
+        height="40"
+        width="40"
+        alt="Logo"
+      />
+    </div>
+    <div class="title">Please sign in to use VTalk</div>
+    <form>
+      <div class="fieldContainer">
+        <div class="inputTitle">Name</div>
+        <div class="inputContainer">
+          <input
+            class="inputField"
+            required
+            @change="userName = $event.target.value"
+          />
+        </div>
+      </div>
+      <div class="fieldContainer">
+        <div class="inputTitle">Profile Image URL</div>
+        <div class="inputContainer">
+          <input class="inputField" required />
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import { ref, toRef } from "vue";
+
+export default {
+  name: "SignUp",
+  props: ["registered", "name", "url"],
+  setup(props) {
+    const userName = toRef(props.name);
+    return {
+      userName,
+    };
+  },
+};
+</script>
+
+<style scoped></style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,27 +1,47 @@
-<template>
-  <div class="bg-[#18191a] min-h-screen duration-[0.5s]">
-    <div class="flex" v-if="registered">
-      <div class="flex-1 flex justify-center  "></div>
-    </div>
-    <div class="flex items-center justify-center w-screen h-[70vh]" v-else></div>
-  </div>
-</template>
-
 <script>
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
+import SignUp from "../components/SignUp.vue";
 
 export default defineComponent({
   name: "IndexPage",
+  setup() {
+    const registered = ref(false);
+    const name = ref("");
+    const url = ref("");
+    const users = ref([]);
+    // expose to template and other options API hooks
+    return {
+      registered,
+    };
+  },
+  mounted() {
+    console.log(this.registered);
+  },
+  components: { SignUp },
 });
 
 // 9RJXi3rAun4YDHzAEDmmVqNaYwdN4JXALrFeDGEjUZFB
 
 //quarter cruel sugar foster cross plunge warm come column double lab eight
 
-
 //New Wallet
 // 3DJdyCwdNKupfNxi6aY1vehpgZ7DeCrzDfXJDdCttT9z
 
 //repeat engage leaf unaware barrel cycle rally truck crazy language rocket deal
-
 </script>
+<template>
+  <div class="wrapper">
+    <div class="homeWrapper" v-if="registered === true">
+      <div class="main">Hello</div>
+    </div>
+    <div class="signupContainer" v-else>
+      <SignUp />
+    </div>
+  </div>
+</template>
+
+<style>
+.center {
+  flex: 1;
+}
+</style>
